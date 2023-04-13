9d3029b0f6370d6f353e3b6f732fbae8305d091b
DaniJFerr <daniel_jferreira@hotmail.com>
2023-04-13 12:27:42 +0100

    upload the js code to fix the issue that the map is not loading/ refresinh the positon on the map when user earch for different ip adress

--- public/script/script.js
+++ public/script/script.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const searchButton = document.getElementById('search-button');
     const ipAddressInput = document.getElementById('search-bar');
@@ -8,38 +6,55 @@ document.addEventListener('DOMContentLoaded', function () {
     const timezoneContainer = document.getElementById('time');
     const ispContainer = document.getElementById('isp');
     const API_KEY = 'at_UcExkLMsteeelpcckDqXrtBwZExkg';
-    const mapDiv = document.getElementById('map'); 
-    
+    const mapDiv = document.getElementById('map');
+    let map;
+    let marker;
+  
     const fetchData = (ipAddress) => {
-      fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ipAddress}`)  
-        .then(response => response.json())  
-        .then(data => { 
-          ipContainer.innerHTML = `<p>IP Address</p><h1>${data.ip}</h1>`; 
+      fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ipAddress}`)
+        .then(response => response.json())
+        .then(data => {
+          ipContainer.innerHTML = `<p>IP Address</p><h1>${data.ip}</h1>`;
           locationContainer.innerHTML = `<p>Location</p><h1>${data.location.city ? data.location.city + ',' : ''} ${data.location.region ? data.location.region + ' ' : ''}${data.location.postalCode || ''}</h1>`;
           timezoneContainer.innerHTML = `<p>Timezone</p><h1>UTC ${data.location.timezone}</h1>`;
           ispContainer.innerHTML = `<p>ISP</p><h1>${data.isp}</h1>`;
-          const map = L.map('map').setView([data.location.lat, data.location.lng], 13);
-          const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19}).addTo(map);
-          const marker = L.marker([data.location.lat, data.location.lng]).addTo(map) // Add a marker at the location coordinates
-          .bindPopup(`<b>${data.location.city}, ${data.location.region} ${data.location.postalCode}</b>`).openPopup(); // Display location info in the popup
-          mapDiv.appendChild(map._container);  
-          
+  
+          if (marker) {
+            map.removeLayer(marker);
+          }
+  
+          map.flyTo([data.location.lat, data.location.lng], 13);
+          marker = L.marker([data.location.lat, data.location.lng]).addTo(map)
+            .bindPopup(`<b>${data.location.city}, ${data.location.region} ${data.location.postalCode}</b>`).openPopup();
         })
         .catch(error => console.log(error));
     };
   
+    // Fetch initial location on page load
+    fetchData('');
+  
     const handleClick = () => {
       const ipAddress = ipAddressInput.value;
-      
-      
+  
       if (ipAddress !== '') {
         fetchData(ipAddress);
-        
-        
       } else {
         ipAddressInput.placeholder='Please enter an IP address';
       }
     };
   
-    searchButton.addEventListener('click', handleClick); 
+    searchButton.addEventListener('click', handleClick);
+  
+    // Initialize map centered at current location
+    map = L.map(mapDiv).setView([0, 0], 13);
+    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19}).addTo(map);
+  
+    // Get current location and set map view
+    map.locate({ setView: true, maxZoom: 13 });
+  
+    // Add marker at current location
+    map.on('locationfound', function(e){
+      marker = L.marker([e.latitude, e.longitude]).addTo(map)
+        .bindPopup("You are here!").openPopup();
+    });
   });
